fbe0ab9537727760df09de2e8cdbdf577f9b69cf
Alexandre Coulon <alex97.coul@gmail.com>
2020-05-09 23:47:41 +0200

    Newsletter form and EmailNewsletter entity created

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\EmailNewsletter;
+use App\Form\NewsletterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -12,7 +16,42 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $newsletterForm = $this
+            ->createForm(NewsletterType::class, null, [
+                'action' => $this->generateUrl('new_email_newsletter')])
+            ->createView();
+
         return $this->render('home/index.html.twig', [
+            'newsletterForm' => $newsletterForm
         ]);
+    }
+
+    /**
+     * @Route("/email-newsletter/new", name="new_email_newsletter")
+     */
+    public function newEmailNewsletter(Request $request, EntityManagerInterface $em)
+    {
+        $form = $this->createForm(NewsletterType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $emailNewsletter = new EmailNewsletter();
+            $emailNewsletter->setEmail($form->getData()['email']);
+            // Todo: gérer les checkboxes (relation Newsletter::news['roleplay'],/ciné... ?) puis update message flash
+
+            $em->persist($emailNewsletter);
+            //Todo: uncomment when database will be created
+//            $em->flush();
+
+//            if ( > 1) {
+//                $this->addFlash('success', 'Vous êtes inscrit aux la newsletters '  );
+//            } else {
+//                $this->addFlash('success', 'Vous êtes inscrit à la newsletter ' . );
+//            }
+//        } else {
+//            $this->addFlash('danger', 'Un problème est survenu lors de l\'enregistrement de votre email);
+        }
+
+        return $this->redirectToRoute('home');
     }
 }
