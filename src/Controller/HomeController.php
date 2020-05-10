304e2ed0009bf953aa586860859211b946f58302
Alexandre Coulon <alex97.coul@gmail.com>
2020-05-10 15:07:26 +0200

    Embebbed newsletter code in separated function

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Form\NewsletterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -16,18 +17,27 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        return $this->render('home/index.html.twig');
+    }
+
+
+    /**
+     * @Route("/newsletter/render", name="newsletter_render")
+     */
+    public function renderNewsletter(): Response
+    {
         $newsletterForm = $this
             ->createForm(NewsletterType::class, null, [
-                'action' => $this->generateUrl('new_email_newsletter')])
+                'action' => $this->generateUrl('newsletter_new_email')])
             ->createView();
 
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/_newsletter.html.twig', [
             'newsletterForm' => $newsletterForm
         ]);
     }
 
     /**
-     * @Route("/email-newsletter/new", name="new_email_newsletter")
+     * @Route("/newsletter/new-email", name="newsletter_new_email")
      */
     public function newEmailNewsletter(Request $request, EntityManagerInterface $em)
     {
